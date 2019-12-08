c992d8ccb09d0448f8c4f20a7af73b165a62f73f
brocolis-developpeur <mallerep@gmail.com>
2019-12-09 00:01:03 +0100

    fix communication with hooks mode

--- src/components/CommunicateV3.js
+++ src/components/CommunicateV3.js
@@ -7,55 +7,42 @@ const Communicate = ({ TTT, setTTT, ws, setWs }) => {
   const [gamestarted, setGameStarted] = useState(false);
   const [theUserName, setTheUserName] = useState(null);
 
-  const handleReceive = event => {
-    const response = JSON.parse(event.data);
-    switch (response.object) {
-      case "newUser":
-        console.log("newuser", response.playersList);
-        setPlayersList(response.playersList);
-        break;
-      case "challenged":
-        setGameStarted(true);
-        alert(`you be been challenged by ${response.by}. You START`);
-        setTTT({
-          ...TTT,
-          opponent: response.by,
-          allowedToPlay: true,
-          player: "X"
-        });
-        break;
-      default:
-        const TTTcopy = { ...TTT };
-        TTTcopy.lastPlayed = [null, null];
-        TTTcopy.allowedToPlay = true;
-        TTTcopy.grid = response.grid;
-        // TTTcopy.scoreX = response.scoreX;
-        // TTTcopy.scoreO = response.scoreO;
-        setTTT(TTTcopy);
-        break;
-    }
-  };
-
   useEffect(() => {
     const connection = () => {
       // this.ws = new WebSocket("wss://backendtictactoe.herokuapp.com/");
-      const NewWs = new WebSocket("ws://localhost:8080");
-
-      setWs(NewWs);
+      const ws = new WebSocket("ws://localhost:8080");
+      ws.addEventListener("message", event => {
+        const response = JSON.parse(event.data);
+        switch (response.object) {
+          case "newUser":
+            console.log("newuser", response.playersList);
+            setPlayersList(response.playersList);
+            break;
+          case "challenged":
+            setGameStarted(true);
+            alert(`you be been challenged by ${response.by}. You START`);
+            setTTT({
+              ...TTT,
+              opponent: response.by,
+              allowedToPlay: true,
+              player: "X"
+            });
+            break;
+          default:
+            const TTTcopy = { ...TTT };
+            TTTcopy.lastPlayed = [null, null];
+            TTTcopy.allowedToPlay = true;
+            TTTcopy.grid = response.grid;
+            // TTTcopy.scoreX = response.scoreX;
+            // TTTcopy.scoreO = response.scoreO;
+            setTTT(TTTcopy);
+            break;
+        }
+      });
+      setWs(ws);
     };
     connection();
   }, []);
-
-  useEffect(() => {
-    if (ws) {
-      ws.addEventListener("message", handleReceive);
-    }
-    return () => {
-      if (ws) {
-        ws.removeEventListener("message", handleReceive);
-      }
-    };
-  }, [handleReceive]);
 
   return (
     <>
